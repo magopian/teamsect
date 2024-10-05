759ffcf9e00dd9ec43f0a53674098e213024a5e9
Mathieu Agopian <mathieu@agopian.info>
2024-10-05 18:50:50 +0200

    Fix resizing

--- pointer/finger.gd
+++ pointer/finger.gd
@@ -7,7 +7,6 @@ class_name Finger extends RigidBody2D
 func _ready() -> void:
 	var sprite: Sprite2D = get_node("Sprite2D")
 	sprite_size = sprite.texture.get_size()
-	EventBus.pricked.connect(_on_pricked)
 
 
 func get_anchor_position() -> Vector2:
@@ -16,11 +15,7 @@ func get_anchor_position() -> Vector2:
 func _physics_process(_delta: float) -> void:
 	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	var mouse_position: Vector2 = get_global_mouse_position()
-	var window_size: Vector2 = get_window().size as Vector2
-	mouse_position.x = clamp(mouse_position.x, sprite_size.x / 2, window_size.x - (sprite_size.x/2))
-	mouse_position.y = clamp(mouse_position.y, sprite_size.y / 2, window_size.y - (sprite_size.y / 2))
+	var viewport_size: Vector2 = get_viewport_rect().size as Vector2
+	mouse_position.x = clamp(mouse_position.x, sprite_size.x / 2, viewport_size.x - (sprite_size.x/2))
+	mouse_position.y = clamp(mouse_position.y, sprite_size.y / 2, viewport_size.y - (sprite_size.y / 2))
 	position = mouse_position
-
-
-func _on_pricked() -> void:
-	print("pricked !!")
