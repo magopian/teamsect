db292e93cff744cb357fde7611cf242e519fa610
Mathieu Agopian <mathieu@agopian.info>
2024-10-06 01:04:42 +0200

    Bandaid glows when thumb pricked, stops when fixed

--- finish/bandaid.gd
+++ finish/bandaid.gd
@@ -1,6 +1,17 @@
 extends Area2D
 
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+
+func _ready() -> void:
+	EventBus.pricked.connect(_on_pricked)
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Blood drop":
+		animation_player.play("RESET")
 		EventBus.blood_fixed.emit(body)
+
+
+func _on_pricked() -> void:
+	animation_player.play("glow")
