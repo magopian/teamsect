86e520b5946d142f797e6cda315f7cc0463000b7
Mathieu Agopian <mathieu@agopian.info>
2024-10-06 21:16:27 +0200

    Add levels up to frenchie

--- dangling/_dangling.gd
+++ dangling/_dangling.gd
@@ -1,8 +1,6 @@
 class_name Dangling extends RigidBody2D
 
-@onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var win_here: Sprite2D = %WinHere
-@onready var child: CollisionShape2D
 
 @export var next_target: RigidBody2D
 @export var current_target: bool = false
@@ -16,10 +14,6 @@ func _process(_delta: float) -> void:
 	if not is_inside_tree():
 		return
 	win_here.visible = current_target
-	if current_target:
-		animation_player.play("glow")
-	else:
-		animation_player.play("RESET")
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -29,7 +23,6 @@ func _on_body_entered(body: Node2D) -> void:
 		set_deferred("contact_monitor", false)
 		EventBus.new_dangling.emit(self)
 		set_deferred("gravity_scale", 8)
-		animation_player.play("RESET")
 		win_here.hide()
 		EventBus.target_reached.emit(self, body)
 		if not next_target:
