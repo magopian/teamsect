0c9fff2a1c36303f764ea5417af3290bd47b9a8a
Mathieu Agopian <mathieu@agopian.info>
2024-10-06 21:54:15 +0200

    Remove tween warning

--- blinker.gd
+++ blinker.gd
@@ -4,8 +4,10 @@ extends Node2D
 @onready var target: Node2D
 
 func _ready() -> void:
+	if not is_inside_tree():
+		return
 	target = get_parent()
-	var tween: Tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
+	var tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
 	tween.set_loops()  # Loop indefinitely
 	tween.tween_property(target, "modulate:a", 0, 0.25)
 	tween.tween_property(target, "modulate:a", 1, 0.25)
