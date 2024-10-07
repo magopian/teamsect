8685dbd7ea347543777112e1d1366640f968b1bb
Mathieu Agopian <mathieu@agopian.info>
2024-10-07 17:12:17 +0200

    Changement the danglings a bit between the last couple of frenchie levels

--- EventBus.gd
+++ EventBus.gd
@@ -11,3 +11,5 @@ signal next
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene_to_file("res://start_menu.tscn")
